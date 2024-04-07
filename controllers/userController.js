3734d87fab43d6599cea10072dd28654d11af1c7
Dip <dipkumardas2002@gmail.com>
2024-04-08 02:21:07 +0530

    logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,6 +48,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .status(201)
     .cookie("token", "", {
       httpOnly: true,
+      secure: true,
+      sameSite:'none',
       expires: new Date(Date.now()),
     })
     .json({
